08c4e6054d5539716539d3954c31ba432bba8b47
Patryk Michalik <contact@patrykmichalik.com>
2022-01-27 15:49:06 +0100

    Add `com.github.lawnchairlauncher:oss-notices` dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -61,4 +61,5 @@ dependencies {
     kapt("com.google.dagger:hilt-compiler:2.40.5")
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.4.0")
     implementation("androidx.hilt:hilt-navigation-compose:1.0.0")
+    implementation("com.github.LawnchairLauncher:oss-notices:1.0.2")
 }
