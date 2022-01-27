8d7a66773d95eb3caf8f272feb72e951802510c9
Patryk Michalik <contact@patrykmichalik.com>
2022-01-27 15:06:01 +0100

    Use AGP 7.1.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,8 +5,8 @@ buildscript {
 }
 
 plugins {
-    id("com.android.application") version "7.1.0-rc01" apply false
-    id("com.android.library") version "7.1.0-rc01" apply false
+    id("com.android.application") version "7.1.0" apply false
+    id("com.android.library") version "7.1.0" apply false
     id("org.jetbrains.kotlin.android") version Versions.KOTLIN apply false
 }
 
