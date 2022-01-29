24312d4c9c27dbf065ea22b5aa310c126e50b6a1
Patryk Michalik <contact@patrykmichalik.com>
2022-01-29 16:58:47 +0100

    `SearchBar`: Customize `DropdownMenu` background color

--- app/src/main/kotlin/app/lawnchair/lawnicons/SearchBar.kt
+++ app/src/main/kotlin/app/lawnchair/lawnicons/SearchBar.kt
@@ -1,9 +1,11 @@
 package app.lawnchair.lawnicons
 
 import androidx.compose.animation.Crossfade
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.material.ContentAlpha
 import androidx.compose.material.DropdownMenu
@@ -11,13 +13,11 @@ import androidx.compose.material.DropdownMenuItem
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.Clear
 import androidx.compose.material.icons.rounded.MoreVert
-import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.focus.FocusRequester
 import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.focus.onFocusChanged
@@ -83,6 +83,7 @@ fun SearchBar(
                     onClick = { dropdownMenuExpanded = true },
                 )
                 DropdownMenu(
+                    modifier = Modifier.background(MaterialTheme.colorScheme.surfaceColorAtElevation(Elevation.Level2)),
                     expanded = dropdownMenuExpanded,
                     onDismissRequest = { dropdownMenuExpanded = false },
                     offset = DpOffset(
