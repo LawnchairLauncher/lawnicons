cb96aad28b72bac3c7e62cfd2a74f4ddf9b68167
Patryk Michalik <contact@patrykmichalik.com>
2022-01-30 11:41:05 +0100

    `About`: Add top bar

--- app/src/main/kotlin/app/lawnchair/lawnicons/ui/destination/About.kt
+++ app/src/main/kotlin/app/lawnchair/lawnicons/ui/destination/About.kt
@@ -1,9 +1,42 @@
 package app.lawnchair.lawnicons.ui.destination
 
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.ArrowBack
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import app.lawnchair.lawnicons.R
+import app.lawnchair.lawnicons.ui.component.ClickableIcon
+import app.lawnchair.lawnicons.ui.component.TopBarWithInsets
 
 @Composable
+@OptIn(ExperimentalMaterial3Api::class)
 fun About(navController: NavController) {
+    val scrollBehavior = remember { TopAppBarDefaults.pinnedScrollBehavior() }
 
+    Scaffold(
+        modifier = Modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+        topBar = {
+            TopBarWithInsets(
+                scrollBehavior = scrollBehavior,
+                title = stringResource(id = R.string.about),
+                navigationIcon = {
+                    ClickableIcon(
+                        onClick = { navController.popBackStack() },
+                        imageVector = Icons.Rounded.ArrowBack,
+                        size = 40.dp,
+                        modifier = Modifier.padding(horizontal = 4.dp),
+                    )
+                },
+            )
+        }
+    ) { }
 }
