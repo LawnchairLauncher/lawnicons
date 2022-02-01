c271728f1c69dba96d8fc388167ce73dcd6ce3ac
Patryk Michalik <contact@patrykmichalik.com>
2022-02-01 08:34:22 +0100

    Update version name

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
         minSdk = 26
         targetSdk = 31
         versionCode = 1
-        versionName = "1.0"
+        versionName = "1.0.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
             useSupportLibrary = true
