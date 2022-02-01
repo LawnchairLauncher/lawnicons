9158250408f9531868444048b6c5f2214d91d173
Patryk Michalik <contact@patrykmichalik.com>
2022-02-01 18:03:31 +0100

    `Contributors.kt`: Optimize imports

--- app/src/main/kotlin/app/lawnchair/lawnicons/ui/destination/Contributors.kt
+++ app/src/main/kotlin/app/lawnchair/lawnicons/ui/destination/Contributors.kt
@@ -2,17 +2,17 @@ package app.lawnchair.lawnicons.ui.destination
 
 import android.content.Intent
 import android.net.Uri
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.ArrowBack
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.platform.LocalContext
