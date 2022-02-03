e86004e2748b091b30cf545f7906d23640c79033
Patryk Michalik <contact@patrykmichalik.com>
2022-02-03 15:48:30 +0100

    Turn on code shrinking

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,7 +23,7 @@ android {
 
     buildTypes {
         release {
-            isMinifyEnabled = false
+            isMinifyEnabled = true
             proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
         }
     }
