8a6179886048bc2c2863842c084298b5b9349537
Patryk Michalik <contact@patrykmichalik.com>
2022-02-04 11:03:33 +0100

    `ApiModule`: Avoid unnecessarily creating multiple `Retrofit` instances

--- app/src/main/kotlin/app/lawnchair/lawnicons/api/ApiModule.kt
+++ app/src/main/kotlin/app/lawnchair/lawnicons/api/ApiModule.kt
@@ -7,12 +7,14 @@ import dagger.hilt.components.SingletonComponent
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.create
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 class ApiModule {
 
     @Provides
+    @Singleton
     fun providesGitHubContributorsApi(): GitHubContributorsAPI {
         return Retrofit.Builder()
             .baseUrl("https://api.github.com/")
