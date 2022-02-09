20a149ca61b201d6b14bc648804c661c35e60c4f
RKBDI <radekxmen.bledowski@gmail.com>
2022-02-09 18:58:07 +0100

    Fix aosp messages icon

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,7 +13,7 @@ android {
         applicationId = "app.lawnchair.lawnicons"
         minSdk = 26
         targetSdk = 31
-        versionCode = 1
+        versionCode = 2
         versionName = "1.0.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
