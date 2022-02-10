0b83518e359acbd4b7dbd3be52453f570a13ad03
RKBDI <radekxmen.bledowski@gmail.com>
2022-02-10 15:25:10 +0100

    Update
    Fixes, redesigns and more icons!

--- app/src/main/kotlin/app/lawnchair/lawnicons/api/GitHubContributorsAPI.kt
+++ app/src/main/kotlin/app/lawnchair/lawnicons/api/GitHubContributorsAPI.kt
@@ -5,6 +5,6 @@ import retrofit2.http.GET
 
 interface GitHubContributorsAPI {
 
-    @GET("repos/LawnchairLauncher/lawnicons/contributors")
+    @GET("repos/RadekBledowski/rkicons/contributors")
     suspend fun getContributors(): List<GitHubContributor>
 }
