d40d2d2cf449c579dd127014a5ac1b1521170ac5
RadekBledowski <radekxmen.bledowski@gmail.com>
2022-06-25 19:27:09 +0200

    small fix
    fixed some apps that are not themed

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
         minSdk = 26
         targetSdk = 31
         versionCode = 2
-        versionName = "1.3.0"
+        versionName = "1.3.1"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
             useSupportLibrary = true
