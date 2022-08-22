7bb828bf075fa3fbd8884d4d657b32ec7ead45f2
SARRAF-5757 <sarraf5757@gmail.com>
2022-08-22 15:02:22 -0400

    catch up to build #267 + Led strip application, MiUI Earth and Mars super wallpaper

--- find_duplicates.py
+++
@@ -1,6 +0,0 @@
-from xml.etree.ElementTree import parse
-
-root = parse("app/src/main/res/xml/grayscale_icon_map.xml").getroot()
-packages = [i.attrib["package"] for i in root]
-duplicates = {i for i in packages if packages.count(i) > 1}
-print(duplicates)
