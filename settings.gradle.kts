bc8e9a521c8b264d882d94ed2efb91f5c9cdcccd
Goooler <wangzongler@gmail.com>
2022-09-18 15:18:23 +0800

    Enable gradle enterprise

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -14,6 +14,19 @@ pluginManagement {
     }
 }
 
+// https://docs.gradle.com/enterprise/gradle-plugin/
+plugins {
+    id("com.gradle.enterprise") version "3.11.1"
+}
+
+gradleEnterprise {
+    buildScan {
+        termsOfServiceUrl = "https://gradle.com/terms-of-service"
+        termsOfServiceAgree = "yes"
+        publishAlways()
+    }
+}
+
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
