b5d98e7bbf608e040614d077bde6b65967ff096b
Goooler <wangzongler@gmail.com>
2023-01-05 09:38:47 +0800

    Bump oss-licenses-plugin to 0.10.6
    https://developers.google.com/android/guides/releases#december_05_2022

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,18 +4,12 @@ plugins {
     id("org.jetbrains.kotlin.android") version "1.7.20" apply false
     id("com.sergei-lapin.napt") version "1.19" apply false
     id("com.google.dagger.hilt.android") version "2.44.2" apply false
-    id("com.google.android.gms.oss-licenses-plugin") version "0.10.5" apply false
+    id("com.google.android.gms.oss-licenses-plugin") version "0.10.6" apply false
     id("org.jmailen.kotlinter") version "3.13.0" apply false
 }
 
 allprojects {
     apply(plugin = "org.jmailen.kotlinter")
-
-    tasks.matching {
-        it.name.contains("OssLicensesTask")
-    }.configureEach {
-        notCompatibleWithConfigurationCache("https://github.com/google/play-services-plugins/issues/206")
-    }
 }
 
 tasks.register("clean") {
