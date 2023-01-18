c11334579040daed0d00bd6f4f41d4bb89ed387d
Goooler <wangzongler@gmail.com>
2023-01-18 10:49:31 +0800

    Kotlin 1.8 and Compose compiler 1.4.0
    https://github.com/JetBrains/kotlin/releases/tag/v1.8.0
    https://developer.android.com/jetpack/androidx/releases/compose-compiler#1.4.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     id("com.android.application") version "7.4.0" apply false
     id("com.android.library") version "7.4.0" apply false
-    id("org.jetbrains.kotlin.android") version "1.7.20" apply false
+    id("org.jetbrains.kotlin.android") version "1.8.0" apply false
     id("com.sergei-lapin.napt") version "1.19" apply false
     id("com.google.dagger.hilt.android") version "2.44.2" apply false
     id("com.google.android.gms.oss-licenses-plugin") version "0.10.6" apply false
