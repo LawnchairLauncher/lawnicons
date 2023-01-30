9e30edc45a0303bd4ea26e5d47dc3f048c69278a
Naveen <naveen.ccmsd@gmail.com>
2023-01-30 20:16:23 +0530

    Enhancement on icon pack  (#996)
    * Add Citi
    
    * link samsung digital wellbeing
    
    * add textra
    
    * Add shareme
    
    * Add moodle
    
    * Add mobikwik
    
    * Add Free and Flasher
    
    * Add Zepto and bigbasket
    
    * Add coinDCX
    
    * Update grayscale_icon_map.xml
    
    * Fixed review comments
    
    * Update flasher.svg
    
    * Update flasher.xml
    
    * Removed Calendar Icons
    
    * Add freecharge,blackberry and few more
    
    * fix typo
    
    * Update content size to 144 for square
    
    * Update flipkart.svg
    
    * Update flipkart.xml
    
    * fix monochrome size issue
    
    * fix monochrome size issue
    
    * add icon request and irctc icons
    
    * fix review commnets
    
    * Add mapping for lawnicons light
    
    * Iconpack core (#3)
    
    * format code
    
    * remove extra line
    
    * Fix monochrome icon size
    
    * Background from true black to grey color
    
    * Remove changes related to varient
    
    * Update on workflow file
    
    ---------
    
    Co-authored-by: Patryk Radziszewski <100310118+Chefski@users.noreply.github.com>

--- svg-processor/src/main/kotlin/app/lawnchair/lawnicons/helper/Application.kt
+++ svg-processor/src/main/kotlin/app/lawnchair/lawnicons/helper/Application.kt
@@ -3,16 +3,14 @@ package app.lawnchair.lawnicons.helper
 fun main() {
     val rootDir = ".."
     val sourceDir = "$rootDir/svgs/"
-    val darkResDir = "$rootDir/app/src/dark/res"
-    val lightResDir = "$rootDir/app/src/light/res"
+    val resDir = "$rootDir/app/src/runtime/res"
     val appFilterFile = "$rootDir/app/assets/appfilter.xml"
 
     // Convert svg to drawable in runtime
-    SvgFilesProcessor.process(sourceDir, "$darkResDir/drawable", "dark")
-    SvgFilesProcessor.process(sourceDir, "$lightResDir/drawable", "light")
+    SvgFilesProcessor.process(sourceDir, "$resDir/drawable")
 
     // Read appfilter xml and create icon, drawable xml file.
-    ConfigProcessor.loadAndCreateConfigs(appFilterFile, darkResDir, lightResDir)
+    ConfigProcessor.loadAndCreateConfigs(appFilterFile, resDir)
 
     println("SvgToVector task completed")
 }
