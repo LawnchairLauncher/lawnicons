ad218ab5a41a230a5a3834e78f9d0a3fb398b7f4
Patryk Radziszewski <100310118+Chefski@users.noreply.github.com>
2023-03-07 17:28:38 +0000

    Fix find_duplicates.py & remove some duplicates (#1090)
    * Optimize 30 SVG(s)
    
    * Fix find_duplicates.py & remove some duplicates
    
    ---------
    
    Co-authored-by: Chefski <Chefski@users.noreply.github.com>

--- find_duplicates.py
+++ find_duplicates.py
@@ -1,6 +1,8 @@
 from xml.etree.ElementTree import parse
 
-root = parse("app/src/main/res/xml/grayscale_icon_map.xml").getroot()
-packages = [i.attrib["package"] for i in root]
+root = parse("app/assets/appfilter.xml").getroot()
+packages = [i.attrib["component"] for i in root]
 duplicates = {i for i in packages if packages.count(i) > 1}
-print(duplicates)
+
+for i in duplicates:
+    print(i)
