8a31043e3f26923a3ad4fd321a7df7482e078444
Goooler <wangzongler@gmail.com>
2023-08-12 14:45:07 +0800

    Replace ConverterFactory

--- app/src/main/kotlin/app/lawnchair/lawnicons/api/ApiModule.kt
+++ app/src/main/kotlin/app/lawnchair/lawnicons/api/ApiModule.kt
@@ -1,12 +1,14 @@
 package app.lawnchair.lawnicons.api
 
+import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import javax.inject.Singleton
+import kotlinx.serialization.json.Json
+import okhttp3.MediaType.Companion.toMediaType
 import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.create
 
 @Module
@@ -16,9 +18,10 @@ class ApiModule {
     @Provides
     @Singleton
     fun providesGitHubContributorsApi(): GitHubContributorsAPI {
+        val contentType = "application/json".toMediaType()
         return Retrofit.Builder()
             .baseUrl("https://api.github.com/")
-            .addConverterFactory(GsonConverterFactory.create())
+            .addConverterFactory(Json.asConverterFactory(contentType))
             .build()
             .create()
     }
