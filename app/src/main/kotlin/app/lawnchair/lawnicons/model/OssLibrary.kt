c770b20f82fe6b216a80a2d873c2390eb473c0ed
Goooler <wangzongler@gmail.com>
2023-08-12 20:48:19 +0800

    Remove unused fields

--- app/src/main/kotlin/app/lawnchair/lawnicons/model/OssLibrary.kt
+++ app/src/main/kotlin/app/lawnchair/lawnicons/model/OssLibrary.kt
@@ -6,19 +6,12 @@ import kotlinx.serialization.Serializable
 data class OssLibrary(
     val groupId: String,
     val artifactId: String,
-    val version: String,
     val name: String,
-    val scm: Scm? = null,
     val spdxLicenses: List<License>? = null,
     val unknownLicenses: List<License>? = null,
 ) {
     @Serializable
     data class License(
-        val identifier: String? = null,
-        val name: String,
         val url: String,
     )
-
-    @Serializable
-    data class Scm(val url: String)
 }
