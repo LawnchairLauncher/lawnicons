fab382a10690d2d410ccbdde7bb1e661c7254f15
Patryk Radziszewski <100310118+Chefski@users.noreply.github.com>
2023-09-21 21:28:02 +0100

    Bump version name & code

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,7 +23,7 @@ val ciRunNumber = providers.environmentVariable("GITHUB_RUN_NUMBER").orNull.orEm
 val isReleaseBuild = ciBuild && ciRef.contains("main")
 val devReleaseName = if (ciBuild) "(Dev #$ciRunNumber)" else "($buildCommit)"
 
-val version = "2.2.1"
+val version = "2.3.0"
 val versionDisplayName = "$version ${if (isReleaseBuild) "" else devReleaseName}"
 
 android {
@@ -34,7 +34,7 @@ android {
         applicationId = "app.lawnchair.lawnicons"
         minSdk = 26
         targetSdk = 34
-        versionCode = 5
+        versionCode = 6
         versionName = versionDisplayName
         vectorDrawables.useSupportLibrary = true
     }
