3db1fb0987da4739012fbf075d44127cc6af2109
Goooler <wangzongler@gmail.com>
2023-12-09 16:09:33 +0800

    Apply kotlinx-collections-immutable

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -157,4 +157,5 @@ dependencies {
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.jakewharton.retrofit:retrofit2-kotlinx-serialization-converter:1.0.0")
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.2")
+    implementation("org.jetbrains.kotlinx:kotlinx-collections-immutable:0.3.6")
 }
