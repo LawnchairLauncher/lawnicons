cbb64377dde56856857a0ae714cff90e1fd4b13a
Goooler <wangzongler@gmail.com>
2023-12-20 14:32:21 +0800

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -124,7 +124,7 @@ licensee {
 
 dependencies {
     val lifecycleVersion = "2.6.2"
-    val hiltVersion = "2.49"
+    val hiltVersion = "2.50"
 
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("androidx.core:core-ktx:1.12.0")
