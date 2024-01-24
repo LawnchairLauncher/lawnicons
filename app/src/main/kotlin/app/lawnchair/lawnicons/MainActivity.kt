c19af874fcbc194ff78a65aa4c0ff4adb3ca4ec8
Zongle Wang <wangzongler@gmail.com>
2024-01-24 15:42:51 +0800

    Enable the edge-to-edge display (#1779)
    https://developer.android.com/develop/ui/views/layout/edge-to-edge#enable-edge-to-edge-display

--- app/src/main/kotlin/app/lawnchair/lawnicons/MainActivity.kt
+++ app/src/main/kotlin/app/lawnchair/lawnicons/MainActivity.kt
@@ -3,12 +3,12 @@ package app.lawnchair.lawnicons
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.windowsizeclass.ExperimentalMaterial3WindowSizeClassApi
 import androidx.compose.material3.windowsizeclass.calculateWindowSizeClass
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
-import androidx.core.view.WindowCompat
 import app.lawnchair.lawnicons.ui.components.Lawnicons
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -20,7 +20,7 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         installSplashScreen()
         super.onCreate(savedInstanceState)
-        WindowCompat.setDecorFitsSystemWindows(window, false)
+        enableEdgeToEdge()
         setContent {
             val windowSizeClass = calculateWindowSizeClass(this)
             Lawnicons(windowSizeClass)
