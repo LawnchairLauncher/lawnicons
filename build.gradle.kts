0af80650c410ac34996e73466f7b33e9bbe0b7a3
Ahmed A. Khalifa <ahmedKhalifa090@gmail.com>
2024-04-17 11:02:42 +0300

    Updating dependency versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ import com.diffplug.gradle.spotless.SpotlessExtension
 import com.diffplug.spotless.extra.wtp.EclipseWtpFormatterStep
 
 plugins {
-    id("com.android.application") version "8.3.1" apply false
+    id("com.android.application") version "8.3.2" apply false
     id("org.jetbrains.kotlin.android") version "1.9.23" apply false
     id("org.jetbrains.kotlin.plugin.serialization") version "1.9.23" apply false
     id("com.google.devtools.ksp") version "1.9.23-1.0.19" apply false
