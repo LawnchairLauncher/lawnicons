3b05a0f564fb149bf056601542d433ae79eb043e
SuperDragonXD <70206496+SuperDragonXD@users.noreply.github.com>
2024-07-27 19:06:51 +0800

    Move icon picker constants

--- app/src/main/kotlin/app/lawnchair/lawnicons/ui/util/Constants.kt
+++ app/src/main/kotlin/app/lawnchair/lawnicons/ui/util/Constants.kt
@@ -4,6 +4,8 @@ object Constants {
     const val ICON_REQUEST_FORM = "https://forms.gle/xt7sJhgWEasuo9TR9"
     const val GITHUB = "https://github.com/LawnchairLauncher/lawnicons"
 
+    const val ICON_PICKER_INTENT_ACTION = "com.novalauncher.THEME"
+
     /**
      * Maximum length of a direct Google forms link. The limit is around 2300-2400 characters, so we use the lower limit to prevent any issues.
      */
