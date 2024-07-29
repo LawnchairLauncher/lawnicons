6707f41c83cb6a6186888556815fbf8e8e37dc0b
SuperDragonXD <70206496+SuperDragonXD@users.noreply.github.com>
2024-07-29 16:07:11 +0800

    Add new core contributors

--- app/src/main/kotlin/app/lawnchair/lawnicons/repository/GitHubContributorsRepository.kt
+++ app/src/main/kotlin/app/lawnchair/lawnicons/repository/GitHubContributorsRepository.kt
@@ -4,9 +4,13 @@ import app.lawnchair.lawnicons.api.GitHubContributorsAPI
 import javax.inject.Inject
 
 val coreContributorIds = listOf(
-    8080853,
-    29139614,
-    56888459,
+    29139614, // Remove Patryk from contributors list, as per https://t.me/lawnchairci/1557
+    56888459, // Remove renovate-bot from contributors list, since we don't count botss as contributors
+    8080853, // suphon-t
+    70206496, // SuperDragonXD
+    60105060, // x9136
+    10363352, // Goooler
+    49114212, // Grabstertv
 )
 
 class GitHubContributorsRepository @Inject constructor(
