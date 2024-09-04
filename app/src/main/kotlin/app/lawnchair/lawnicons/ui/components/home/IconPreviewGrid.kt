1292415090e193e6c1dbda37db5d684815234dbd
SuperDragonXD <70206496+SuperDragonXD@users.noreply.github.com>
2024-09-05 00:10:42 +0800

    Add background to scrollbar, highlight icons on scrollbar click
    When selecting the scrollbar, the icons with the same letter will now be scaled (110%) to highlight that they are the same category.
    
    Further improvements can be made after discussion.

--- app/src/main/kotlin/app/lawnchair/lawnicons/ui/components/home/IconPreviewGrid.kt
+++ app/src/main/kotlin/app/lawnchair/lawnicons/ui/components/home/IconPreviewGrid.kt
@@ -1,6 +1,7 @@
 package app.lawnchair.lawnicons.ui.components.home
 
 import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.animation.fadeIn
 import androidx.compose.animation.fadeOut
 import androidx.compose.foundation.ExperimentalFoundationApi
@@ -9,13 +10,14 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.ColumnScope
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.navigationBars
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.statusBarsPadding
@@ -27,15 +29,21 @@ import androidx.compose.foundation.lazy.grid.LazyGridState
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.lazy.grid.rememberLazyGridState
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.derivedStateOf
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.scale
 import androidx.compose.ui.graphics.asImageBitmap
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
@@ -48,7 +56,7 @@ import app.lawnchair.lawnicons.ui.util.SampleData
 import app.lawnchair.lawnicons.ui.util.toPaddingValues
 import app.lawnchair.lawnicons.util.appIcon
 import kotlinx.collections.immutable.ImmutableList
-import my.nanihadesuka.compose.LazyVerticalGridScrollbar
+import my.nanihadesuka.compose.InternalLazyVerticalGridScrollbar
 import my.nanihadesuka.compose.ScrollbarSelectionMode
 import my.nanihadesuka.compose.ScrollbarSettings
 
@@ -63,34 +71,31 @@ fun IconPreviewGrid(
     contentPadding: PaddingValues? = null,
     gridState: LazyGridState = rememberLazyGridState(),
 ) {
-    val indexOfFirstItem = remember { derivedStateOf { gridState.firstVisibleItemIndex } }
-    val letter = iconInfo[indexOfFirstItem.value].label[0].uppercase()
+    val indexOfFirstItem by remember { derivedStateOf { gridState.firstVisibleItemIndex } }
+    val letter = iconInfo[indexOfFirstItem].label[0].uppercase()
+    var thumbSelected by remember { mutableStateOf(false) }
 
     Column(
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center,
         modifier = modifier.fillMaxWidth(),
     ) {
-        LazyVerticalGridScrollbar(
+        val horizontalGridPadding = if (isExpandedScreen) 32.dp else 8.dp
+        Box(
             modifier = Modifier
                 .widthIn(max = 640.dp)
                 .fillMaxWidth()
                 .statusBarsPadding()
                 .then(
-                    if (isExpandedScreen) Modifier.padding(top = 26.dp) else Modifier.padding(bottom = 80.dp),
+                    if (isExpandedScreen) {
+                        Modifier.padding(top = 26.dp)
+                    } else {
+                        Modifier.padding(
+                            bottom = 80.dp,
+                        )
+                    },
                 ),
-            state = gridState,
-            settings = ScrollbarSettings(
-                alwaysShowScrollbar = true,
-                thumbUnselectedColor = MaterialTheme.colorScheme.primaryContainer,
-                thumbSelectedColor = MaterialTheme.colorScheme.primary,
-                selectionMode = ScrollbarSelectionMode.Thumb,
-            ),
-            indicatorContent = { _, isThumbSelected ->
-                ScrollbarIndicator(letter, isThumbSelected)
-            },
         ) {
-            val horizontalGridPadding = if (isExpandedScreen) 32.dp else 8.dp
             LazyVerticalGrid(
                 columns = GridCells.Adaptive(minSize = 80.dp),
                 contentPadding = contentPadding ?: WindowInsets.navigationBars.toPaddingValues(
@@ -106,26 +111,63 @@ fun IconPreviewGrid(
                             GridItemSpan(maxLineSpan)
                         },
                     ) {
-                        TopAppBar()
+                        AppBarListItem()
+                    }
+                    items(
+                        items = iconInfo,
+                        contentType = { "icon_preview" },
+                    ) { iconInfo ->
+                        val scale by animateFloatAsState(
+                            if (thumbSelected && iconInfo.label.first()
+                                    .toString() == letter
+                            ) {
+                                1.1f
+                            } else {
+                                1f
+                            },
+                            label = "",
+                        )
+                        IconPreview(
+                            modifier = Modifier
+                                .scale(scale),
+                            iconInfo = iconInfo,
+                            isIconPicker = isIconPicker,
+                            onSendResult = onSendResult,
+                        )
                     }
                 }
-                items(
-                    items = iconInfo,
-                    contentType = { "icon_preview" },
-                ) { iconInfo ->
-                    IconPreview(
-                        iconInfo = iconInfo,
-                        isIconPicker = isIconPicker,
-                        onSendResult = onSendResult,
-                    )
-                }
+            }
+            Box(
+                contentAlignment = Alignment.CenterEnd,
+            ) {
+                Spacer(
+                    Modifier
+                        .fillMaxHeight()
+                        .width(8.dp)
+                        .background(MaterialTheme.colorScheme.surfaceContainer)
+                        .clip(CircleShape),
+                )
+                InternalLazyVerticalGridScrollbar(
+                    modifier = Modifier.offset(7.dp),
+                    state = gridState,
+                    settings = ScrollbarSettings(
+                        alwaysShowScrollbar = true,
+                        thumbUnselectedColor = MaterialTheme.colorScheme.primary,
+                        thumbSelectedColor = MaterialTheme.colorScheme.primary,
+                        selectionMode = ScrollbarSelectionMode.Thumb,
+                    ),
+                    indicatorContent = { _, isThumbSelected ->
+                        thumbSelected = isThumbSelected
+                        ScrollbarIndicator(letter, isThumbSelected)
+                    },
+                )
             }
         }
     }
 }
 
 @Composable
-private fun ColumnScope.ScrollbarIndicator(
+private fun ScrollbarIndicator(
     label: String,
     isThumbSelected: Boolean,
 ) {
@@ -154,7 +196,7 @@ private fun ColumnScope.ScrollbarIndicator(
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun TopAppBar(modifier: Modifier = Modifier) {
+private fun AppBarListItem(modifier: Modifier = Modifier) {
     val context = LocalContext.current
     CenterAlignedTopAppBar(
         modifier = modifier,
