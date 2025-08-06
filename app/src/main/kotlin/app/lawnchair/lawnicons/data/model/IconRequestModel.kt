55ca89de4979ea1d051882494ffe66c549fe5972
SuperDragonXD <70206496+SuperDragonXD@users.noreply.github.com>
2025-08-06 15:14:00 +0800

    feat(iconrequest): implement viewmodel

--- app/src/main/kotlin/app/lawnchair/lawnicons/data/model/IconRequestModel.kt
+++ app/src/main/kotlin/app/lawnchair/lawnicons/data/model/IconRequestModel.kt
@@ -1,6 +1,19 @@
 package app.lawnchair.lawnicons.data.model
 
+import java.io.File
+
+/**
+ * Represents a request for a list of system icons.
+ *
+ * @property list A list of [SystemIconInfo] objects representing the icons.
+ * @property iconCount The total number of icons in the list.
+ */
 data class IconRequestModel(
     val list: List<SystemIconInfo>,
     val iconCount: Int,
+)
+
+data class IconRequestData(
+    val zipFile: File,
+    val componentListString: String,
 )
