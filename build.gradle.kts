654b7c1626d9832d014173289f5729fe8ce6b084
Zongle Wang <wangzongler@gmail.com>
2025-12-16 15:33:33 +0800

    Simplify version catalog (#3232)
    * Inline versions
    
    * Simplify libraries
    
    * Tweak plugins
    
    * Cleanups
    
    * Explicit version for AGP

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,6 +6,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
+    alias(libs.plugins.kotlin.jvm) apply false
     alias(libs.plugins.kotlin.compose) apply false
     alias(libs.plugins.kotlin.serialization) apply false
     alias(libs.plugins.ksp) apply false
