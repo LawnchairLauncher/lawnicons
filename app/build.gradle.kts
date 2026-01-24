278f3c692c4f59f7af8ff2b7e81c221008817a85
Goooler <wangzongler@gmail.com>
2026-01-24 14:22:12 +0800

    Remove KSP

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,6 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.compose)
     alias(libs.plugins.kotlin.serialization)
-    alias(libs.plugins.ksp)
     alias(libs.plugins.licensee)
     alias(libs.plugins.metro)
 }
