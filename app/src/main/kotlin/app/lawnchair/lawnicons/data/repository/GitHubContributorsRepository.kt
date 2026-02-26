45541e055c095cafea22914ab2930d8b5294219e
Zongle Wang <wangzongler@gmail.com>
2026-02-26 12:34:15 +0800

    Tweak core contributor ids (#3449)

--- app/src/main/kotlin/app/lawnchair/lawnicons/data/repository/GitHubContributorsRepository.kt
+++ app/src/main/kotlin/app/lawnchair/lawnicons/data/repository/GitHubContributorsRepository.kt
@@ -2,33 +2,9 @@ package app.lawnchair.lawnicons.data.repository
 
 import app.lawnchair.lawnicons.LawniconsScope
 import app.lawnchair.lawnicons.data.api.GitHubContributorsAPI
+import app.lawnchair.lawnicons.ui.destination.about.coreContributorIds
 import dev.zacsweers.metro.Inject
 import dev.zacsweers.metro.SingleIn
-
-val coreContributorIds = listOf(
-    // Remove Patryk from contributors list, as per https://t.me/lawnchairci/1557
-    29139614,
-    // Remove renovate-bot from contributors list, since we don't count bots as contributors
-    56888459,
-    // GitHub Actions bot
-    41898282,
-    // suphon-t
-    8080853,
-    // SuperDragonXD
-    70206496,
-    // Chefski
-    100310118,
-    // x9136
-    60105060,
-    // Goooler
-    10363352,
-    // Grabstertv
-    49114212,
-    // Copilot
-    198982749,
-    // Copilot
-    175728472,
-)
 
 @SingleIn(LawniconsScope::class)
 @Inject
